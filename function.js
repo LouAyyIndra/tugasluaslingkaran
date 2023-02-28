612b374980ca3c57a938495221008be13f377982
LouAyyIndra <118791712+LouAyyIndra@users.noreply.github.com>
2023-02-28 11:53:56 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+var phi;
+var jari2;
+var total;
+
+function jumlahluaslingkaran(a) {
+    jari2= a**2 
+phi=3.14;
+    total = phi * jari2;
+    return total 
+}
+
+alert(jumlahluaslingkaran(10));
